409160367912fc30b18d809286a9b3dc69054df7
Aspectre <correa.jc04@gmail.com>
2026-03-15 21:45:56 -0400

    Adicao do Botao de criacao de formulario no Mapa e Legenda

--- src/app/(tabs)/dashboard/mapa.tsx
+++ src/app/(tabs)/dashboard/mapa.tsx
@@ -1,9 +1,11 @@
-import {Text, View} from 'react-native';
+import {Text, TouchableOpacity, View} from 'react-native';
 import { mapaStyle } from '@/src/styles/mapaStyles';
 import MapView, {Marker} from 'react-native-maps';
+import { useRouter } from "expo-router";
 
 
 export default function Mapa() {
+    const router = useRouter();
     return (
         <View style={mapaStyle.container}>
             <MapView style={mapaStyle.mapa} initialRegion={{latitude:-3.1190, longitude: -60.0217, latitudeDelta: 0.05, longitudeDelta: 0.05}} scrollEnabled={true} zoomEnabled={true}>
@@ -11,8 +13,19 @@ export default function Mapa() {
                     title="Ponto inicial"
                     description="Exemplo de marcador"
                 />
-            </MapView>               
+            </MapView>
+            <View style={mapaStyle.legenda}>
+                <Text>Pendente</Text>
+                <Text>Em Coleta</Text>
+                <Text>Resolvido</Text>
+            </View>
+
+            <View style={mapaStyle.viewBotao}>
+                <TouchableOpacity style={mapaStyle.botao} 
+                onPress={() => router.push('/(tabs)/dashboard/reportes')}><Text>+</Text></TouchableOpacity>
+            </View>                
         </View>
+
         
     )
 }
--- src/styles/mapaStyles.js
+++ src/styles/mapaStyles.js
@@ -4,9 +4,32 @@ export const mapaStyle = StyleSheet.create({
     container: {
         flex:1,
         
+        
     },
     mapa:{
         flex: 1,
         
+    },
+    legenda: {
+        position: "absolute",
+        borderRadius: 12,
+        backgroundColor: "#fff",
+        bottom: 20,
+        left: 20,
+        padding: 10
+    },
+    botao: {
+        backgroundColor: "green",
+        width: 80,
+        height: 80,
+        borderRadius: 40,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    viewBotao: {
+        position: "absolute",
+        bottom: 20,
+        right: 20,
+        padding: 1
     }
 })
