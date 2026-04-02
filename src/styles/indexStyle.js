63fdcc88459845952a4c30a5d6f01578621e7482
reisgms <reis.tfate@gmail.com>
2026-04-01 20:43:22 -0400

    adicionando estlilo a tela index

--- src/styles/indexStyle.js
+++ src/styles/indexStyle.js
@@ -5,5 +5,50 @@ export const indexStyle = StyleSheet.create({
         flex:1,
         alignItems: 'center',
         justifyContent: 'center',
+    
+
+    },
+    logoContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+    },
+    logo: { 
+        width: 200,
+        height: 200,
+    },
+    logoText:{
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 10,
+    },
+
+    botao1:{
+        height: 50,
+        width: 100,
+        padding: 10,
+        backgroundColor: 'green',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 10,
+        borderRadius: 15,
+        
+    },
+    botao1Text:{
+        color: 'white',
+        fontWeight: 'bold',
+    },
+    botao2:{
+        height: 50,
+        width: 100,
+        padding: 10,
+        backgroundColor: 'green',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 15,
+    },
+    botao2Text:{
+        color: 'white',
+        fontWeight: 'bold',
     },
 })
